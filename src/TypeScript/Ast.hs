2de27d71bf4159cd591cb12f3a964e7f524b0437
iliyangermanov <iliyan.germanov971@gmail.com>
2025-12-22 01:05:43 +0200

    WIP: TS AST model

--- src/TypeScript/Ast.hs
+++ src/TypeScript/Ast.hs
@@ -2,4 +2,14 @@ module TypeScript.Ast where
 
 import Data.Text
 
-data TsAst = Import {raw :: Text}
+data TsAst
+  = Import
+      { path :: FilePath
+      , raw :: Text
+      }
+  | Comment
+      { text :: Text
+      , raw :: Text
+      }
+  | Source {raw :: Text}
+  deriving (Show, Eq)
