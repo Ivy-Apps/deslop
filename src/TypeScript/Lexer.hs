9d77c415e1f914a0f2f80646432b8b7db48198c0
iliyangermanov <iliyan.germanov971@gmail.com>
2025-12-23 17:40:49 +0200

    WIP: Make the Lexer idomatic and correct

--- src/TypeScript/Lexer.hs
+++ src/TypeScript/Lexer.hs
@@ -4,7 +4,7 @@ import Data.Text
 import Data.Text qualified as T
 import Data.Void
 import Text.Megaparsec
-import Text.Megaparsec.Char (char, string)
+import Text.Megaparsec.Char
 import TypeScript.Tokens
 
 type Lexer = Parsec Void Text
@@ -14,37 +14,29 @@ lexer = many pToken <* eof
 
 pToken :: Lexer TsToken
 pToken =
-  choice
-    [ try pImport
-    , try pComment
-    , pRaw
-    ]
+    choice
+        [ try pImport
+        , try pComment
+        , pRaw
+        ]
 
 pImport :: Lexer TsToken
-pImport = do
-  start <- string "import"
-  (content, end) <- manyTill_ anySingle pImportEnd
-  let raw = start <> T.pack content <> T.singleton end
-  return ImportTok {raw = raw}
- where
-  pImportEnd = choice [char ';', char '\n', char ')']
+pImport = ImportTok . fst <$> match (string "import" <* manyTill anySingle end)
+  where
+    end = choice [try $ string ";\n", string ";", string "\n", string ")"]
 
 pComment :: Lexer TsToken
-pComment = fail "WIP"
+pComment = pLineComment
 
 pLineComment :: Lexer TsToken
 pLineComment = do
-  prefix <- string "//"
-  comment <- takeWhileP (Just "comment content") ((/=) '\n')
-  return CommentTok {comment = comment, raw = prefix <> comment <> T.singleton '\n'}
+    prefix <- string "//"
+    comment <- takeWhileP (Just "comment content") ((/=) '\n')
+    return CommentTok {comment = comment, raw = prefix <> comment <> T.singleton '\n'}
 
 pRaw :: Lexer TsToken
 pRaw = do
-  c <- anySingle
-  rest <- takeWhileP Nothing (not . knownStart)
-  return RawTok {raw = T.singleton c <> rest}
- where
-  knownStart :: Char -> Bool
-  knownStart '/' = True
-  knownStart 'i' = True
-  knownStart _ = False
+    (raw, _) <- match (anySingle >> manyTill anySingle (lookAhead (atTokenStart <|> eof)))
+    return RawTok {raw = raw}
+  where
+    atTokenStart = () <$ string "//" <|> () <$ string "import"
