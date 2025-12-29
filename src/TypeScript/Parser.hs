e110305ca21a75ffec64ea8b9aa5f89f9918caa7
iliyangermanov <iliyan.germanov971@gmail.com>
2025-12-29 19:46:24 +0200

    WIP: Import parser

--- src/TypeScript/Parser.hs
+++ src/TypeScript/Parser.hs
@@ -1,13 +1,19 @@
 module TypeScript.Parser (
     parseTs,
+    parseImport,
 ) where
 
 import Data.Bifunctor
-import Data.Text
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Void
 import Text.Megaparsec
+import Text.Megaparsec.Char
 import TypeScript.AST
 import TypeScript.Lexer
 import TypeScript.Tokens
+
+type Parser = Parsec Void Text
 
 data TsFile = TsFile
     { path :: FilePath
@@ -28,4 +34,28 @@ buildAST = fmap node
     node (TsToken r WhitespaceK) = Source r
     node (TsToken r (CommentK c)) = Comment {raw = r, content = c}
     node (TsToken r (DocsK c)) = Docs {raw = r, content = c}
-    node (TsToken r ImportK) = undefined
+    node (TsToken r ImportK) = case parseImport r of
+        Left _ -> Source r
+        Right n -> n
+
+parseImport :: Text -> Either String TsNode
+parseImport = first errorBundlePretty . runParser parser ""
+  where
+    -- import type { User, Permissions } from "./types";
+    parser :: Parser TsNode
+    parser = do
+        p1 <- T.pack <$> manyTill anySingle (string "from")
+        p2 <- string "from"
+        p3 <- mconcat <$> some (string " ")
+        p4 <- pQuote
+        t <- manyTill anySingle (char p4)
+        s <- takeRest
+        pure
+            Import
+                { prefix = p1 <> p2 <> p3 <> T.singleton p4
+                , target = T.pack t
+                , suffix = s
+                }
+
+pQuote :: Parser Char
+pQuote = char '\'' <|> char '"'
