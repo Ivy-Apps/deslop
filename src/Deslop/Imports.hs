1170b9d5f78d4ec6e16d0ada47346a346620c32b
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-01 18:08:05 +0200

    WIP: Imports

--- src/Deslop/Imports.hs
+++ src/Deslop/Imports.hs
@@ -1,30 +1,31 @@
 module Deslop.Imports where
 
+import Data.Bifunctor
 import Data.List
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Effectful
 import Effectful.Reader.Static
-import Effectful.Reader.Static (ask)
 import Effects.FileSystem
 import TypeScript.AST
 import TypeScript.Config
 
 importAliases :: (Reader TsConfig :> es, FileSystem :> es) => TsProgram -> Eff es TsProgram
 importAliases prog = do
-    pure $ prog {ast = fixImport <$> prog.ast}
+    ast' <- traverse fixImport prog.ast
+    pure prog {ast = ast'}
   where
-    fixImport og@(Import _ t _) = og
-    fixImport n = n
+    fixImport og@(Import _ t _) = do
+        t' <- fixTarget t
+        pure og {target = t'}
+    fixImport x = pure x
 
-    fixTarget :: (FileSystem :> es, Reader TsConfig :> es) => Text -> Eff es Text
-    fixTarget t = do
-        fp <- T.pack <$> fullPath (T.unpack t)
-        cfg <- ask @TsConfig
-        pure $ fpWithAlias cfg fp
+    fixTarget t =
+        uncurry fpWithAlias . bimap T.pack (.paths)
+            <$> ((,) <$> fullPath (T.unpack t) <*> ask @TsConfig)
 
-    fpWithAlias cfg fp =
-        fromMaybe fp $
-            (\(ImportAlias l p) -> T.replace p l fp)
-                <$> find ((`T.isInfixOf` fp) . (.path)) cfg.paths
+    fpWithAlias fp =
+        fromMaybe fp
+            . fmap (\(ImportAlias l p) -> T.replace p l fp)
+            . find ((`T.isInfixOf` fp) . (.path))
