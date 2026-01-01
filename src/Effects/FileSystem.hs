8709761f7da4d65cd9a69637b1ee34ca953b37f5
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-01 12:41:39 +0200

    Add "fileExists" method to FileSystem

--- src/Effects/FileSystem.hs
+++ src/Effects/FileSystem.hs
@@ -1,18 +1,21 @@
 module Effects.FileSystem (
-  readFileBS,
-  writeFileBS,
-  FileSystem (ReadFile, WriteFile),
-  runFileSystemIO,
+    readFileBS,
+    writeFileBS,
+    fileExists,
+    FileSystem (ReadFile, WriteFile, FileExists),
+    runFileSystemIO,
 ) where
 
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
 import Effectful
 import Effectful.Dispatch.Dynamic (interpret, send)
+import System.Directory (doesFileExist)
 
 data FileSystem :: Effect where
-  ReadFile :: FilePath -> FileSystem m ByteString
-  WriteFile :: FilePath -> ByteString -> FileSystem m ()
+    ReadFile :: FilePath -> FileSystem m ByteString
+    WriteFile :: FilePath -> ByteString -> FileSystem m ()
+    FileExists :: FilePath -> FileSystem m Bool
 
 type instance DispatchOf FileSystem = Dynamic
 
@@ -22,7 +25,11 @@ readFileBS = send . ReadFile
 writeFileBS :: (FileSystem :> es) => FilePath -> ByteString -> Eff es ()
 writeFileBS path content = send $ WriteFile path content
 
+fileExists :: (FileSystem :> es) => FilePath -> Eff es Bool
+fileExists = send . FileExists
+
 runFileSystemIO :: (IOE :> es) => Eff (FileSystem : es) a -> Eff es a
 runFileSystemIO = interpret $ \_env -> \case
-  ReadFile path -> liftIO $ BS.readFile path
-  WriteFile path content -> liftIO $ BS.writeFile path content
+    ReadFile path -> liftIO $ BS.readFile path
+    WriteFile path content -> liftIO $ BS.writeFile path content
+    FileExists path -> liftIO $ doesFileExist path
--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -5,13 +5,14 @@ import Data.ByteString qualified as BS
 import Data.IORef
 import Effectful
 import Effectful.Dispatch.Dynamic
-import Effects.FileSystem (FileSystem (ReadFile, WriteFile))
+import Effects.FileSystem (FileSystem (FileExists, ReadFile, WriteFile))
 
 runFileSystemTest ::
-  (IOE :> es) =>
-  IORef (Maybe ByteString) ->
-  Eff (FileSystem : es) a ->
-  Eff es a
+    (IOE :> es) =>
+    IORef (Maybe ByteString) ->
+    Eff (FileSystem : es) a ->
+    Eff es a
 runFileSystemTest ref = interpret $ \_ -> \case
-  ReadFile path -> liftIO $ BS.readFile path
-  WriteFile _path content -> liftIO $ writeIORef ref (Just content)
+    ReadFile path -> liftIO $ BS.readFile path
+    WriteFile _path content -> liftIO $ writeIORef ref (Just content)
+    FileExists _path -> pure True
