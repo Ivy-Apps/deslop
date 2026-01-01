be3e2483be1548ddb8218e3cd7ba2716bdfcbd3e
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-01 22:15:07 +0200

    Add unit tests

--- test/Deslop/ImportSpec.hs
+++ test/Deslop/ImportSpec.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
 module Deslop.ImportSpec where
 
 import Deslop.Imports (importAliases)
@@ -10,7 +12,7 @@ import TypeScript.Config
 spec :: Spec
 spec = do
     describe "Happy path" $ do
-        it "'Convers relative import to '@/'" $ do
+        it "'converts '../../' relative import to '@/'" $ do
             -- Given
             let cfg = TsConfig [ImportAlias "@/" "src/"]
             let prog =
@@ -29,10 +31,88 @@ spec = do
             prog' <- runEff . runReader cfg $ importAliases prog
 
             -- Then
-            prog.path `shouldBe` prog'.path
             head prog'.ast
                 `shouldBe` Import
                     { prefix = "import * from '"
                     , target = "@/lib/welcome"
+                    , suffix = "';\n"
+                    }
+
+        it "'converts './' relative import to '@/*'" $ do
+            -- Given
+            let cfg = TsConfig [ImportAlias "@/" "src/"]
+            let prog =
+                    TsModule
+                        { path = "src/features/home/home.ts"
+                        , ast =
+                            [ Import
+                                { prefix = "import * from '"
+                                , target = "./useHomeViewModel"
+                                , suffix = "';\n"
+                                }
+                            ]
+                        }
+
+            -- When
+            prog' <- runEff . runReader cfg $ importAliases prog
+
+            -- Then
+            head prog'.ast
+                `shouldBe` Import
+                    { prefix = "import * from '"
+                    , target = "@/features/home/useHomeViewModel"
+                    , suffix = "';\n"
+                    }
+
+        it "does not touch bare specifiers - e.g. 'react'" $ do
+            -- Given
+            let cfg = TsConfig [ImportAlias "@/" "src/"]
+            let prog =
+                    TsModule
+                        { path = "src/features/home/home.ts"
+                        , ast =
+                            [ Import
+                                { prefix = "import * from '"
+                                , target = "react"
+                                , suffix = "';\n"
+                                }
+                            ]
+                        }
+
+            -- When
+            prog' <- runEff . runReader cfg $ importAliases prog
+
+            -- Then
+            prog `shouldBe` prog'
+
+        it "converts more complex '@test/*' imports" $ do
+            -- Given
+            let cfg =
+                    TsConfig
+                        { paths =
+                            [ ImportAlias "@test/" "tests/",
+                            ImportAlias "@/" "src/"
+                            ]
+                        }
+            let prog =
+                    TsModule
+                        { path = "src/features/auth.spec.ts"
+                        , ast =
+                            [ Import
+                                { prefix = "import * from '"
+                                , target = "../../tests/auth-fixture"
+                                , suffix = "';\n"
+                                }
+                            ]
+                        }
+
+            -- When
+            prog' <- runEff . runReader cfg $ importAliases prog
+
+            -- Then
+            head prog'.ast
+                `shouldBe` Import
+                    { prefix = "import * from '"
+                    , target = "@test/auth-fixture"
                     , suffix = "';\n"
                     }
