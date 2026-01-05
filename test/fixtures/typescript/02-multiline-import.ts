d09c6345ec87dee4745eaf7ba91635a2380b9c92
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-05 12:48:42 +0200

    Add multiline import test

---
+++ test/fixtures/typescript/02-multiline-import.ts
@@ -0,0 +1,14 @@
+import {
+    itemsPerPage,
+    itemInfo,
+} from '../../lib/shop/constants';
+import * from 'react';
+import {
+    a,
+    b,
+    c // not sure if we need that!
+} from './abc'
+
+export function ItemsPage() {
+    return <ItemsView />
+}
