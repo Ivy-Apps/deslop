3037cebab3a004812a9787d6b4c1aa9b8beb93cf
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-06 21:58:55 +0200

    Parse translations

--- src/Translations/Parser.hs
+++ src/Translations/Parser.hs
@@ -6,10 +6,27 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Lazy qualified as BL
 import Data.HashMap.Strict (HashMap)
 import Data.HashMap.Strict qualified as HM
+import Data.List.NonEmpty (NonEmpty, nonEmpty)
+import Data.Maybe (catMaybes)
 import Data.Text (Text)
+import Data.Text qualified as T
 import Effectful (Eff, (:>))
-import Effectful.FileSystem (FileSystem)
-import Effects.FileSystem (RoFileSystem, readFileBS)
+import Effects.FileSystem (RoFileSystem, listDirectory, readFileBS)
+import System.FilePath
+import System.FilePath (dropExtension, splitDirectories)
+import Utils (safeHead)
+
+data Translations = Translations
+    { base :: Translation
+    , extra :: NonEmpty Translation
+    }
+    deriving (Show, Eq)
+
+data Translation = Translation
+    { language :: Text
+    , tree :: TransTree
+    }
+    deriving (Show, Eq)
 
 data TransTree
     = Branch (HashMap Text TransTree)
@@ -22,8 +39,25 @@ instance FromJSON TransTree where
     parseJSON ob@(Object _) = Branch <$> parseJSON ob
     parseJSON e = typeMismatch "String or Object" e
 
+defaultLanguage :: Text
+defaultLanguage = "en"
+
+readTranslations :: (RoFileSystem :> es) => FilePath -> Eff es (Maybe Translations)
+readTranslations root = do
+    ds <- fmap (root </>) <$> listDirectory root
+    ts <- catMaybes <$> traverse readTranslation ds
+    let base = safeHead . filter (isDeault . (.language)) $ ts
+    let extras = nonEmpty . filter (not . isDeault . (.language)) $ ts
+    pure $ Translations <$> base <*> extras
+  where
+    isDeault = (==) defaultLanguage
+
+readTranslation :: (RoFileSystem :> es) => FilePath -> Eff es (Maybe Translation)
+readTranslation path =
+    readFileBS path
+        >>= pure . fmap (Translation language) . parseTransTree
+  where
+    language = T.pack . dropExtension . last . splitDirectories $ path
+
 parseTransTree :: ByteString -> Maybe TransTree
 parseTransTree = decode . BL.fromStrict
-
-readTranslations :: (RoFileSystem :> es) => FilePath -> Eff es (Maybe TransTree)
-readTranslations path = readFileBS path >>= pure . parseTransTree
