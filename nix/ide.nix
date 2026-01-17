a4469572eee84630b71b7f54f3b6c35eb3d56da9
iliyangermanov <iliyan.germanov971@gmail.com>
2026-01-18 01:30:10 +0200

    Add window keymaps

--- nix/ide.nix
+++ nix/ide.nix
@@ -23,6 +23,57 @@
   clipboard.register = "unnamedplus";
 
   keymaps = [
+    # --- Window Management ---
+    {
+      mode = "n";
+      key = "<leader>h";
+      action = "<C-w>h";
+      options.desc = "Focus Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>l";
+      action = "<C-w>l";
+      options.desc = "Focus Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>j";
+      action = "<C-w>j";
+      options.desc = "Focus Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>k";
+      action = "<C-w>k";
+      options.desc = "Focus Up";
+    }
+    # Move the actual windows around
+    {
+      mode = "n";
+      key = "<leader>H";
+      action = "<C-w>H";
+      options.desc = "Move Window Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>L";
+      action = "<C-w>L";
+      options.desc = "Move Window Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>J";
+      action = "<C-w>J";
+      options.desc = "Move Window Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>K";
+      action = "<C-w>K";
+      options.desc = "Move Window Up";
+    }
+
     # --- Haskell Tools ---
     {
       mode = "n";
