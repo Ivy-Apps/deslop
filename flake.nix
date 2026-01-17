392f4c17d2fa95b4216741725b48a586bb4484b6
Iliyan Germanov <iliyan.germanov971@gmail.com>
2026-01-18 01:41:14 +0200

    Fix Nix (#4)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   description = "Deslop: Haskell Env + Immutable Neovim IDE";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    allow-import-from-derivation = true;
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -16,8 +28,7 @@
 
       perSystem = { config, pkgs, system, ... }:
         let
-          # --- 1. Haskell Configuration ---
-          ghcVersion = "ghc910";
+          ghcVersion = "ghc967";
 
           haskellPackages = pkgs.haskell.packages.${ghcVersion}.override {
             overrides = self: super: {
@@ -25,183 +36,16 @@
             };
           };
 
-          # --- 2. Neovim Configuration ---
-          nixvimModule = {
-            colorschemes.catppuccin.enable = true;
-
-            globals = {
-              mapleader = " ";
-              maplocalleader = " ";
-            };
-
-            opts = {
-              number = true;
-              relativenumber = true;
-              shiftwidth = 2;
-              expandtab = true;
-              smartindent = true;
-              breakindent = true;
-              backspace = [ "indent" "eol" "start" ];
-              # Smart Case Search (Case insensitive unless capital used)
-              ignorecase = true;
-              smartcase = true;
-            };
-
-            clipboard.register = "unnamedplus";
-
-            # --- Keymaps ---
-            keymaps = [
-              # --- Haskell Tools ---
-              {
-                mode = "n";
-                key = "<leader>fm";
-                action = "<cmd>lua require('conform').format()<CR>";
-                options.desc = "Format (Fourmolu)";
-              }
-              {
-                mode = "n";
-                key = "<leader>cl";
-                action = "<cmd>!hlint %<CR>";
-                options.desc = "Check Lint (Hlint CLI)";
-              }
-              {
-                mode = "n";
-                key = "<leader>rr";
-                action = "<cmd>lua require('haskell-tools').repl.toggle()<CR>";
-                options.desc = "Haskell REPL";
-              }
-              {
-                mode = "n";
-                key = "<leader>ca";
-                action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
-                options.desc = "Code Actions";
-              }
-
-              # --- Search & UI ---
-              {
-                mode = "n";
-                key = "<leader>ss";
-                action = "<cmd>Telescope current_buffer_fuzzy_find<CR>";
-                options.desc = "Search in Buffer";
-              }
-              {
-                mode = "n";
-                key = "<leader>nh";
-                action = "<cmd>noh<CR>";
-                options.desc = "Clear Highlights";
-              }
-              {
-                mode = "n";
-                key = "<leader>bk";
-                action = "<cmd>bd<CR>";
-                options.desc = "Kill Buffer";
-              }
-              {
-                mode = "n";
-                key = "<C-a>";
-                action = "ggVG";
-                options.desc = "Select All";
-              }
-
-              # --- Git ---
-              {
-                mode = "n";
-                key = "<leader>gs";
-                action = "<cmd>Neogit<CR>";
-                options.desc = "Git Status (Neogit)";
-              }
-              {
-                mode = "n";
-                key = "<leader>hp";
-                action = "<cmd>Gitsigns preview_hunk<CR>";
-                options.desc = "Preview Git Hunk";
-              }
-            ];
-
-            plugins = {
-              web-devicons.enable = true;
-              nvim-tree.enable = true;
-
-              # --- FIX: Enable diffview so Neogit can use it ---
-              diffview.enable = true;
-
-              neogit = {
-                enable = true;
-                settings.integrations.diffview = true;
-              };
-
-              telescope = {
-                enable = true;
-                keymaps = {
-                  "<leader>ff" = "find_files";
-                  "<leader>fg" = "live_grep";
-                };
-              };
-
-              treesitter = {
-                enable = true;
-                settings.indent.enable = false;
-                grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [ haskell json yaml markdown nix bash make ];
-              };
-
-              haskell-tools = {
-                enable = true;
-                settings.tools = {
-                  hover.enable = true;
-                  log.level = "info";
-                };
-              };
-
-              conform-nvim = {
-                enable = true;
-                settings = {
-                  format_on_save = { timeout_ms = 5000; lsp_fallback = true; };
-                  formatters_by_ft = {
-                    haskell = [ "fourmolu" ];
-                    nix = [ "nixpkgs_fmt" ];
-                  };
-                };
-              };
-
-              cmp = {
-                enable = true;
-                autoEnableSources = true;
-                settings = {
-                  sources = [{ name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } { name = "luasnip"; }];
-                  mapping = {
-                    "<CR>" = "cmp.mapping.confirm({ select = true })";
-                    "<Tab>" = "cmp.mapping.select_next_item()";
-                  };
-                };
-              };
-              luasnip.enable = true;
-
-              lsp.enable = true;
-              lsp.servers.nil_ls.enable = true;
-
-              gitsigns.enable = true;
-              comment.enable = true;
-              lualine.enable = true;
-            };
-
-            extraPackages = [
-              pkgs.ripgrep
-              pkgs.fd
-              pkgs.nixpkgs-fmt
-              haskellPackages.haskell-language-server
-              haskellPackages.hoogle
-              pkgs.fourmolu
-              pkgs.hlint
-            ];
+          nixvimConfig = import ./nix/ide.nix { 
+            inherit pkgs haskellPackages; 
           };
+          
+          nvim = nixvim.legacyPackages.${system}.makeNixvim nixvimConfig;
 
-          nvim = nixvim.legacyPackages.${system}.makeNixvim nixvimModule;
-
-          # --- 3. Shell Definitions ---
           ciDeps = [
             haskellPackages.cabal-install
             haskellPackages.hspec-discover
-            pkgs.hlint
+            haskellPackages.hlint
             pkgs.git
           ];
 
@@ -219,7 +63,7 @@
             ci = haskellPackages.shellFor {
               packages = p: [ p.deslop ];
               nativeBuildInputs = ciDeps;
-              buildInputs = [ pkgs.zlib ];
+              buildInputs = [ pkgs.zlib pkgs.xz ];
             };
 
             default = haskellPackages.shellFor {
@@ -229,12 +73,18 @@
               nativeBuildInputs = ciDeps ++ localDeps;
               buildInputs = [
                 pkgs.zlib
+                pkgs.xz
                 haskellPackages.haskell-language-server
-                pkgs.fourmolu
+                haskellPackages.fourmolu
               ];
 
               shellHook = ''
-                echo "🔮 Deslop IDE (GHC 9.10)"
+                echo "🔮 Deslop Dev env initialized."
+                echo "--------------------------------------------------------"
+                # This will print the exact versions currently in the PATH
+                echo "✅ GHC:  $(ghc --version)"
+                echo "✅ HLS:  $(haskell-language-server --version | awk '{print $1, $2, $3, $4, $5}')"
+                echo "--------------------------------------------------------"
               
                 if [ ! -f hie.yaml ]; then
                   echo "   Generating hie.yaml for HLS..."
@@ -249,4 +99,3 @@
         };
     };
 }
-
---
+++ nix/ide.nix
@@ -0,0 +1,223 @@
+{ pkgs, haskellPackages }:
+
+{
+  colorschemes.catppuccin.enable = true;
+
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+
+  opts = {
+    # --- Standard Vim Options ---
+    number = true;
+    relativenumber = true;
+    shiftwidth = 2;
+    expandtab = true;
+    smartindent = true;
+    breakindent = true;
+    ignorecase = true;
+    smartcase = true;
+    
+    # --- Window/Split Behavior ---
+    splitbelow = true; # New horizontal splits go down
+    splitright = true; # New vertical splits go right
+    termguicolors = true; 
+  };
+
+  clipboard.register = "unnamedplus";
+
+  keymaps = [
+    # --- Window Management ---
+    {
+      mode = "n";
+      key = "<leader>h";
+      action = "<C-w>h";
+      options.desc = "Focus Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>l";
+      action = "<C-w>l";
+      options.desc = "Focus Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>j";
+      action = "<C-w>j";
+      options.desc = "Focus Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>k";
+      action = "<C-w>k";
+      options.desc = "Focus Up";
+    }
+    # Move the actual windows around
+    {
+      mode = "n";
+      key = "<leader>H";
+      action = "<C-w>H";
+      options.desc = "Move Window Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>L";
+      action = "<C-w>L";
+      options.desc = "Move Window Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>J";
+      action = "<C-w>J";
+      options.desc = "Move Window Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>K";
+      action = "<C-w>K";
+      options.desc = "Move Window Up";
+    }
+
+    # --- Haskell Tools ---
+    {
+      mode = "n";
+      key = "<leader>fm";
+      action = "<cmd>lua require('conform').format()<CR>";
+      options.desc = "Format (Fourmolu)";
+    }
+    {
+      mode = "n";
+      key = "<leader>cl";
+      action = "<cmd>!hlint %<CR>";
+      options.desc = "Check Lint (Hlint CLI)";
+    }
+    {
+      mode = "n";
+      key = "<leader>rr";
+      action = "<cmd>lua require('haskell-tools').repl.toggle()<CR>";
+      options.desc = "Haskell REPL";
+    }
+    {
+      mode = "n";
+      key = "<leader>ca";
+      action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
+      options.desc = "Code Actions";
+    }
+    # --- Search & UI ---
+    {
+      mode = "n";
+      key = "<leader>ss";
+      action = "<cmd>Telescope current_buffer_fuzzy_find<CR>";
+      options.desc = "Search in Buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>nh";
+      action = "<cmd>noh<CR>";
+      options.desc = "Clear Highlights";
+    }
+    {
+      mode = "n";
+      key = "<leader>bk";
+      action = "<cmd>bd<CR>";
+      options.desc = "Kill Buffer";
+    }
+    # --- Git ---
+    {
+      mode = "n";
+      key = "<leader>gs";
+      action = "<cmd>Neogit<CR>";
+      options.desc = "Git Status (Neogit)";
+    }
+    {
+      mode = "n";
+      key = "<leader>hp";
+      action = "<cmd>Gitsigns preview_hunk<CR>";
+      options.desc = "Preview Git Hunk";
+    }
+  ];
+
+  plugins = {
+    web-devicons.enable = true;
+    nvim-tree.enable = true;
+    diffview.enable = true;
+
+    neogit = {
+      enable = true;
+      settings.integrations.diffview = true;
+    };
+
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>ff" = "find_files";
+        "<leader>fg" = "live_grep";
+      };
+    };
+
+    treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = false;
+      };
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [ 
+        haskell 
+        json 
+        yaml 
+        markdown 
+        nix 
+        bash 
+        make 
+      ];
+    };
+
+    haskell-tools = {
+      enable = true;
+      settings.tools = {
+        hover.enable = true;
+        log.level = "info";
+      };
+    };
+
+    conform-nvim = {
+      enable = true;
+      settings = {
+        format_on_save = { timeout_ms = 5000; lsp_fallback = true; };
+        formatters_by_ft = {
+          haskell = [ "fourmolu" ];
+          nix = [ "nixpkgs_fmt" ];
+        };
+      };
+    };
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [{ name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } { name = "luasnip"; }];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+        };
+      };
+    };
+    luasnip.enable = true;
+    lsp.enable = true;
+    lsp.servers.nil_ls.enable = true;
+    gitsigns.enable = true;
+    comment.enable = true;
+    lualine.enable = true;
+  };
+
+  extraPackages = [
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.nixpkgs-fmt
+    haskellPackages.haskell-language-server
+    haskellPackages.hoogle
+    haskellPackages.fourmolu
+    haskellPackages.hlint
+  ];
+}
