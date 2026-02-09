57c1bc95970ccf089270f1ed0297976e18d086f9
iliyangermanov <iliyan.germanov971@gmail.com>
2026-02-09 17:07:09 +0200

    Add prompt golden test

---
+++ test/Translations/TranslatorSpec.hs
@@ -0,0 +1,20 @@
+module Translations.TranslatorSpec where
+
+import Data.Text (Text)
+import Data.Text qualified as T
+import Test.Hspec
+import Test.Hspec.Golden (defaultGolden)
+import Translations.Translator
+
+enMessages :: [(Text, Text)]
+enMessages =
+    [ ("errors.title", "Something went wrong")
+    , ("errors.message", "Try again or contact support.")
+    , ("errors.cta", "Try again")
+    ]
+
+spec :: Spec
+spec = describe "Prompt" $ do
+        it "EN to ES" $ do
+            let p = translatePrompt "en" "es" enMessages
+            defaultGolden "prompt-en-es" (T.unpack p)
