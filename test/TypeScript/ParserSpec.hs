497c6fc5af391131bf680e12d0c0a5327d08ec8f
Iliyan Germanov <iliyan.germanov971@gmail.com>
2026-02-15 04:06:56 +0200

    Fix imports (#13)

---
+++ test/TypeScript/ParserSpec.hs
@@ -0,0 +1,66 @@
+module TypeScript.ParserSpec where
+
+import Control.Monad
+import Data.Text (Text)
+import Data.Text qualified as T
+import Test.Hspec
+import TypeScript.AST
+import TypeScript.Parser
+
+spec :: Spec
+spec = do
+    describe "Imports" $ do
+        let cases =
+                [
+                    ( "import * from '@/lib/utils'"
+                    , Import
+                        { prefix = "import * from '"
+                        , target = "@/lib/utils"
+                        , suffix = "'"
+                        }
+                    )
+                ,
+                    ( "import { \"hello\" as hell } from \"./Context\"\n"
+                    , Import
+                        { prefix = "import { \"hello\" as hell } from \""
+                        , target = "./Context"
+                        , suffix = "\"\n"
+                        }
+                    )
+                ,
+                    ( "import '../../tests/viewmodel-test';"
+                    , Import
+                        { prefix = "import '"
+                        , target = "../../tests/viewmodel-test"
+                        , suffix = "';"
+                        }
+                    )
+                ,
+                    ( "await import ('../heavy-module');"
+                    , Import
+                        { prefix = "import ('"
+                        , target = "../heavy-module"
+                        , suffix = "');"
+                        }
+                    )
+                ,
+                    ( "await import ('../../lib/extra').extras;"
+                    , Import
+                        { prefix = "import ('"
+                        , target = "../../lib/extra"
+                        , suffix = "')"
+                        }
+                    )
+                ]
+        forM_ cases $ \(input, expected) ->
+            it input $ do
+                let file = TsFile "test.ts" (T.pack input)
+                case parseTs file of
+                    Left err -> expectationFailure err
+                    Right program -> do
+                        let importsOnly = filter isImport program.ast
+                        importsOnly `shouldBe` [expected]
+
+isImport :: TsNode -> Bool
+isImport Import {} = True
+isImport _ = False
