0ce2b0dd430de1e794be6a4100d17182ef802cae
iliyangermanov <iliyan.germanov971@gmail.com>
2026-02-21 20:08:41 +0200

    Make unit tests more readable

--- test/ParamsSpec.hs
+++ test/ParamsSpec.hs
@@ -1,13 +1,20 @@
 module ParamsSpec where
 
+import Options.Applicative
 import Params (Params (..), optsInfo, parserPrefs)
 import Test.Hspec
-import Options.Applicative
 
 spec :: Spec
 spec = describe "parseParams" $ do
     it "defaults project path to . and both flags to False when given no args" $ do
-        parseParams [] `shouldBe` Just (Params "." False False)
+        parseParams []
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = False
+                    , checkMode = False
+                    }
+                )
 
     it "parses PROJECT_PATH as sole positional argument" $ do
         parseParams ["/some/ts/project"]
@@ -19,35 +26,107 @@ spec = describe "parseParams" $ do
                     }
                 )
         parseParams ["."]
-            `shouldBe` Just (Params "." False False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = False
+                    , checkMode = False
+                    }
+                )
         parseParams ["src"]
-            `shouldBe` Just (Params "src" False False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "src"
+                    , modifiedOnly = False
+                    , checkMode = False
+                    }
+                )
 
     it "parses --modified / -m and sets modifiedOnly to True" $ do
         parseParams ["--modified"]
-            `shouldBe` Just (Params "." True False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = True
+                    , checkMode = False
+                    }
+                )
         parseParams ["-m"]
-            `shouldBe` Just (Params "." True False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = True
+                    , checkMode = False
+                    }
+                )
         parseParams ["/path", "--modified"]
-            `shouldBe` Just (Params "/path" True False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "/path"
+                    , modifiedOnly = True
+                    , checkMode = False
+                    }
+                )
         parseParams ["--modified", "other/dir"]
-            `shouldBe` Just (Params "other/dir" True False)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "other/dir"
+                    , modifiedOnly = True
+                    , checkMode = False
+                    }
+                )
 
     it "parses --check / -c and sets checkMode to True" $ do
         parseParams ["--check"]
-            `shouldBe` Just (Params "." False True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = False
+                    , checkMode = True
+                    }
+                )
         parseParams ["-c"]
-            `shouldBe` Just (Params "." False True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "."
+                    , modifiedOnly = False
+                    , checkMode = True
+                    }
+                )
         parseParams ["/path", "--check"]
-            `shouldBe` Just (Params "/path" False True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "/path"
+                    , modifiedOnly = False
+                    , checkMode = True
+                    }
+                )
 
     it "parses combination of path, --modified and --check" $ do
         parseParams ["my-project", "--modified", "--check"]
-            `shouldBe` Just (Params "my-project" True True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "my-project"
+                    , modifiedOnly = True
+                    , checkMode = True
+                    }
+                )
         parseParams ["my-project", "-m", "-c"]
-            `shouldBe` Just (Params "my-project" True True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "my-project"
+                    , modifiedOnly = True
+                    , checkMode = True
+                    }
+                )
         parseParams ["--modified", "--check", "lib"]
-            `shouldBe` Just (Params "lib" True True)
+            `shouldBe` Just
+                ( Params
+                    { projectPath = "lib"
+                    , modifiedOnly = True
+                    , checkMode = True
+                    }
+                )
 
     it "returns Nothing for --help" $ do
         parseParams ["--help"] `shouldBe` Nothing
