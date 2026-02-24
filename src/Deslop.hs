044a297641b99b94ecd49a4c465ba053cfca3973
iliyangermanov <iliyan.germanov971@gmail.com>
2026-02-24 16:08:27 +0200

    Refactor

--- src/Deslop.hs
+++ src/Deslop.hs
@@ -41,6 +41,7 @@ import Effects.Git
 import Effects.ReportProblem (ReportProblem, getProblems, runReportProblem)
 import Params
 import System.Directory (getHomeDirectory)
+import System.Exit (exitFailure)
 import System.FilePath
 import Translations.Manager
 import Translations.Parser
@@ -50,6 +51,11 @@ import TypeScript.Parser (TsFile (TsFile, content, path), parseTs)
 import Types
 import UI
 
+getSecretsPath :: IO FilePath
+getSecretsPath = do
+    home <- getHomeDirectory
+    pure $ home </> ".deslop" </> "secrets.json"
+
 runDeslop :: Params -> IO ()
 runDeslop params =
     getSecretsPath
@@ -58,6 +64,7 @@ runDeslop params =
   where
     run secrets = do
         start <- getCurrentTime
+
         runEff
             . runFileSystemIO
             . runCLILog
@@ -72,12 +79,9 @@ runDeslop params =
         let seconds = realToFrac diff :: Double
         printTime seconds
 
-    handleInitError err = printErr . T.pack $ show err
-
-getSecretsPath :: IO FilePath
-getSecretsPath = do
-    home <- getHomeDirectory
-    pure $ home </> ".deslop" </> "secrets.json"
+    handleInitError err = do
+        printErr . T.pack . show $ err
+        exitFailure
 
 doWork ::
     ( WrFileSystem :> es
@@ -106,59 +110,6 @@ doWork params _ = do
 
     unless params.checkMode (doTranslations params)
 
-doTranslations ::
-    ( WrFileSystem :> es
-    , RoFileSystem :> es
-    , Git :> es
-    , IOE :> es
-    , AI :> es
-    , CLILog :> es
-    , Concurrent :> es
-    , ReportProblem :> es
-    ) =>
-    Params -> Eff es ()
-doTranslations params = do
-    liftIO . putStrLn $ "Translating..."
-    translateRes <- runErrorNoCallStack @TranslationsError (translateProject params)
-    case translateRes of
-        Left err -> liftIO . printErr . T.pack $ show err
-        Right _ -> liftIO . putStrLn $ "Translations success."
-
-getSecrets :: (RoFileSystem :> es) => FilePath -> Eff es (Either InitError Secrets)
-getSecrets sp =
-    fileExists sp
-        >>= bool (pure $ Left SecretsMissing) readSecrets
-  where
-    readSecrets =
-        readFileBS sp
-            <&> first (SecretsJsonError . T.pack)
-                . eitherDecode @Secrets
-                . BL.fromStrict
-
-translateProject ::
-    ( WrFileSystem :> es
-    , RoFileSystem :> es
-    , CLILog :> es
-    , AI :> es
-    , Concurrent :> es
-    , Error TranslationsError :> es
-    ) =>
-    Params ->
-    Eff es ()
-translateProject params =
-    readTranslations translationsPath
-        >>= maybe handleReadError pipeline
-  where
-    pipeline ts = fixTranslations ts >>= either handleTranslateErorr writeTranslations
-    writeTranslations = traverse_ writeTranslation . (.extra)
-    writeTranslation (Translation l t) = writeFileBS (translationFile l) (TE.encodeUtf8 $ render t)
-
-    translationFile l = translationsPath </> (T.unpack l <> ".json")
-    translationsPath = params.projectPath </> "messages"
-
-    handleReadError = throwError ParseTranslationsError
-    handleTranslateErorr = throwError . TranslateError
-
 deslopProject ::
     ( WrFileSystem :> es
     , RoFileSystem :> es
@@ -181,20 +132,6 @@ deslopProject params = do
                 forM_ (mFiles `intersect` (normalise <$> files)) deslopFile
             else
                 forM_ files deslopFile
-
-tsConfig ::
-    ( RoFileSystem :> es
-    , Error DeslopError :> es
-    ) =>
-    FilePath ->
-    Eff es TsConfig
-tsConfig projPath = loadConfig $ projPath </> "tsconfig.json"
-  where
-    loadConfig fp = fileExists fp >>= bool (handleMissing fp) (handleFound fp)
-    handleFound fp = readFileBS fp >>= maybe (handleInvalid fp) pure . parseTsConfig
-
-    handleMissing = throwError . TsConfigNotFoundError
-    handleInvalid = throwError . TsConfigParseError
 
 getTsFiles :: (RoFileSystem :> es) => FilePath -> Eff es [FilePath]
 getTsFiles dir = listDirectory dir >>= fmap concat . traverse (processEntry dir)
@@ -238,3 +175,70 @@ removeSlop p c = fromRight c <$> pipeline
             TsFile {path = p, content = TE.decodeUtf8 c}
     deslop = foldr (>=>) pure [importAliases]
     renderProgram = TE.encodeUtf8 . render . (.ast)
+
+doTranslations ::
+    ( WrFileSystem :> es
+    , RoFileSystem :> es
+    , Git :> es
+    , IOE :> es
+    , AI :> es
+    , CLILog :> es
+    , Concurrent :> es
+    , ReportProblem :> es
+    ) =>
+    Params -> Eff es ()
+doTranslations params = do
+    liftIO . putStrLn $ "Translating..."
+    translateRes <- runErrorNoCallStack @TranslationsError (translateProject params)
+    case translateRes of
+        Left err -> liftIO . printErr . T.pack $ show err
+        Right _ -> liftIO . putStrLn $ "Translations success."
+
+translateProject ::
+    ( WrFileSystem :> es
+    , RoFileSystem :> es
+    , CLILog :> es
+    , AI :> es
+    , Concurrent :> es
+    , Error TranslationsError :> es
+    ) =>
+    Params ->
+    Eff es ()
+translateProject params =
+    readTranslations translationsPath
+        >>= maybe handleReadError pipeline
+  where
+    pipeline ts = fixTranslations ts >>= either handleTranslateErorr writeTranslations
+    writeTranslations = traverse_ writeTranslation . (.extra)
+    writeTranslation (Translation l t) = writeFileBS (translationFile l) (TE.encodeUtf8 $ render t)
+
+    translationFile l = translationsPath </> (T.unpack l <> ".json")
+    translationsPath = params.projectPath </> "messages"
+
+    handleReadError = throwError ParseTranslationsError
+    handleTranslateErorr = throwError . TranslateError
+
+getSecrets :: (RoFileSystem :> es) => FilePath -> Eff es (Either InitError Secrets)
+getSecrets sp =
+    fileExists sp
+        >>= bool (pure $ Left SecretsMissing) readSecrets
+  where
+    readSecrets =
+        readFileBS sp
+            <&> first (SecretsJsonError . T.pack)
+                . eitherDecode @Secrets
+                . BL.fromStrict
+
+tsConfig ::
+    ( RoFileSystem :> es
+    , Error DeslopError :> es
+    ) =>
+    FilePath ->
+    Eff es TsConfig
+tsConfig projPath = loadConfig $ projPath </> "tsconfig.json"
+  where
+    loadConfig fp = fileExists fp >>= bool (handleMissing fp) (handleFound fp)
+    handleFound fp = readFileBS fp >>= maybe (handleInvalid fp) pure . parseTsConfig
+
+    handleMissing = throwError . TsConfigNotFoundError
+    handleInvalid = throwError . TsConfigParseError
