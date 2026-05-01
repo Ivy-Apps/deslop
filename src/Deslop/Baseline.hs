5e67984baf9869f7fac321d2f67decf379380aeb
iliyangermanov <iliyan.germanov971@gmail.com>
2026-05-01 20:26:52 +0300

    WIP: Baseline

--- src/Deslop/Baseline.hs
+++ src/Deslop/Baseline.hs
@@ -1,1 +1,36 @@
-module Deslop.Baseline where
+{-# LANGUAGE QuasiQuotes #-}
+
+module Deslop.Baseline (
+    loadBaseline,
+    loadBaselineFromFile,
+) where
+
+import Data.HashSet qualified as HS
+import Deslop.Problem (ProblemId)
+import Effectful (Eff)
+import Effectful.Internal.Effect ((:>))
+import Effects.FileSystem (AbsPath, RoFileSystem, fsFileExists, fsMkAbsolute, fsReadFile)
+import System.OsPath (OsPath, osp)
+import Utils (todo)
+
+newtype Baseline = Baseline (HashSet ProblemId) deriving (Show, Eq)
+
+emptyBaseline :: Baseline
+emptyBaseline = Baseline HS.empty
+
+baselinePath :: OsPath
+baselinePath = [osp|deslop/baseline.yaml|]
+
+loadBaseline :: (RoFileSystem :> es) => Eff es Baseline
+loadBaseline = fsMkAbsolute baselinePath >>= loadBaselineFromFile
+
+loadBaselineFromFile :: (RoFileSystem :> es) => AbsPath -> Eff es Baseline
+loadBaselineFromFile fp = do
+    exists <- fsFileExists fp
+    if exists
+        then
+            fsReadFile fp >>= pure . fromRight emptyBaseline . parseBasline
+        else pure emptyBaseline
+
+parseBasline :: ByteString -> Either Text Baseline
+parseBasline = todo
